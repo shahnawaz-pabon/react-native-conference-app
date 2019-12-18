b415cfb7073e572a1b15c098d151a67de67fc36d
PabonSEC <s.pabon93@gmail.com>
2019-12-19 00:54:09 +0600

    App's slider added

--- App.js
+++ App.js
@@ -11,26 +11,144 @@ import {
   StyleSheet,
   View,
   Text,
-  Button
+  Button,
+  Image
 } from 'react-native';
 
-export default function App() {
+import AppIntroSlider from 'react-native-app-intro-slider';
 
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
+import Icon from 'react-native-ionicons';
 
-  return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text changed!')}/>
-    </View>
-  );
-};
+const slides = [
+  {
+    key: 'somethun',
+    title: 'Title 1',
+    text: 'Description.\nSay something cool',
+    image: require('./assets/images/slide.png'),
+    backgroundColor: '#59b2ab',
+  },
+  {
+    key: 'somethun-dos',
+    title: 'Title 2',
+    text: 'Other cool stuff',
+    image: require('./assets/images/slide.png'),
+    backgroundColor: '#febe29',
+  },
+  {
+    key: 'somethun1',
+    title: 'Rocket guy',
+    text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+    image: require('./assets/images/slide.png'),
+    backgroundColor: '#22bcb5',
+  }
+];
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showRealApp: false,
+      //To show the main page of the app
+    };
+  }
+  _renderNextButton = () => {
+    return (
+      <View style={styles.buttonCircle}>
+        <Icon
+          name="md-arrow-round-forward"
+          color="rgba(255, 255, 255, .9)"
+          size={24}
+          style={{ backgroundColor: 'transparent' }}
+        />
+      </View>
+    );
+  };
+  _renderDoneButton = () => {
+    return (
+      <View style={styles.buttonCircle}>
+        <Icon
+          name="md-checkmark"
+          color="rgba(255, 255, 255, .9)"
+          size={24}
+          style={{ backgroundColor: 'transparent' }}
+        />
+      </View>
+    );
+  };
+  _onDone = () => {
+    this.setState({ showRealApp: true });
+  };
+  _onSkip = () => {
+    this.setState({ showRealApp: true });
+  };
+  _renderItem = ({ item }) => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: item.backgroundColor,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          paddingBottom: 100
+        }}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Image style={styles.image} source={item.image} />
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
+    );
+  };
+  render() {
+    //If false show the Intro Slides
+    if (this.state.showRealApp) {
+      //Real Application
+      return (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 50,
+          }}>
+          <Text>
+            This will be your screen when you click Skip from any slide or Done
+            button at last
+          </Text>
+        </View>
+      );
+    } else {
+      //Intro slides
+      return (
+        <AppIntroSlider
+          slides={slides}
+          renderItem={this._renderItem}
+          onDone={this._onDone}
+          renderDoneButton={this._renderDoneButton}
+          renderNextButton={this._renderNextButton}
+        />
+      );
+    }
+  }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  text: {
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
+    paddingVertical: 30,
+  },
+  title: {
+    fontSize: 25,
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  buttonCircle: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'rgba(0, 0, 0, .2)',
+    borderRadius: 20,
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
