af2bf936a4d3e2f90c5589fed4fcad8cb2e75a81
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-09-02 23:53:56 +0600

    Example drawer navigator added

---
+++ src/navigators/testdrawer.js
@@ -0,0 +1,12 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
