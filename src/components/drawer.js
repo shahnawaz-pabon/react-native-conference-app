076ac214013f44aae105b95da27ca15cbe389585
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-09-17 00:29:16 +0600

    Avatar added on the drawer

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import {
+    Avatar,
+    Title,
+    Caption,
+    Paragraph,
     Drawer,
     TouchableRipple,
     Switch
@@ -18,6 +22,31 @@ export function DrawerComponent(props) {
         <View style={{ flex: 1 }}>
             <DrawerContentScrollView {...props}>
                 <View style={styles.drawerContent}>
+
+                    {/* Start of Avatar Contents */}
+                    <View>
+                        <Avatar.Image
+                            source={{
+                                uri: 'https://randomuser.me/api/portraits/men/1.jpg',
+                            }}
+                            size={85}
+                            style={styles.userInfoSection}
+                        />
+                        <Title style={[styles.userInfoSection, styles.title]}>John Doe</Title>
+                        <Caption style={[styles.userInfoSection, styles.caption]}>@johndoe</Caption>
+                        <View style={[styles.userInfoSection, styles.row]}>
+                            <View style={styles.section}>
+                                <Paragraph style={[styles.paragraph, styles.caption]}>103</Paragraph>
+                                <Caption style={styles.caption}>Following</Caption>
+                            </View>
+                            <View style={styles.section}>
+                                <Paragraph style={[styles.paragraph, styles.caption]}>169</Paragraph>
+                                <Caption style={styles.caption}>Followers</Caption>
+                            </View>
+                        </View>
+                    </View>
+                    {/* End of Avatar Contents */}
+
                     <Drawer.Section
                         title={<Text style={[styles.drawerItemLabel, styles.sectionTitle]}>CONFERENCE</Text>}
                         style={styles.drawerSection}
@@ -67,7 +96,7 @@ export function DrawerComponent(props) {
                     <Drawer.Section title={<Text style={[styles.drawerItemLabel, styles.sectionTitle]}>PREFERENCES</Text>}>
                         <TouchableRipple onPress={() => { console.log("Pressed") }}>
                             <View style={styles.preference}>
-                                <Text>Dark Theme</Text>
+                                <Text style={styles.drawerItemLabel}>Dark Theme</Text>
                                 <View pointerEvents="none">
                                     <Switch />
                                 </View>
@@ -83,6 +112,31 @@ export function DrawerComponent(props) {
 const styles = StyleSheet.create({
     drawerContent: {
         flex: 1,
+    },
+    userInfoSection: {
+        alignSelf: "center"
+    },
+    title: {
+        marginTop: 10,
+        fontFamily: "Ubuntu-Bold",
+    },
+    caption: {
+        fontSize: 14,
+        lineHeight: 14,
+        fontFamily: "Ubuntu-Bold",
+    },
+    row: {
+        marginTop: 10,
+        flexDirection: 'row'
+    },
+    section: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginRight: 15,
+    },
+    paragraph: {
+        fontFamily: "Ubuntu-Bold",
+        marginRight: 3,
     },
     drawerSection: {
         marginTop: 15
