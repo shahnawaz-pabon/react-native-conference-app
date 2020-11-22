0fa8bdfa0e131d849f43fe1fc92ef8e5ad344f1d
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-11-22 23:47:20 +0600

    Animated view added with some styles

--- src/components/FloatingButton.js
+++ src/components/FloatingButton.js
@@ -2,17 +2,33 @@ import React from 'react';
 import { TouchableWithoutFeedback, Animated, StyleSheet, View, Text } from 'react-native';
 import { Icon } from 'react-native-elements';
 
-const FloatingButton = () => (
-    <View style={styles.container}>
-        <Text>Button</Text>
+const FloatingButton = ({ props }) => (
+    <View style={[styles.container, props.style]}>
+        <TouchableWithoutFeedback>
+            <Animated.View style={[styles.button, styles.menu]}>
+                <Icon
+                    size={24}
+                    color="white"
+                    type="material"
+                    name="notifications"
+                />
+            </Animated.View>
+        </TouchableWithoutFeedback>
     </View>
 )
 
 export const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        position: "absolute"
+    },
+    buttons: {
+        position: "absolute",
+        width: 60,
+        height: 60
+    },
+    menu: {
+        backgroundColor: "#2c3e50"
     }
 });
 
